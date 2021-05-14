2617a915e2ef401bc8a580b743411591b0aff5c8
sahandbabali <sahand.babali@gmail.com>
2021-05-15 03:56:33 +0430

    fir

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from './components/navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-test
+    <div className="App container">
+<Navbar />
+
     </div>
   );
 }
